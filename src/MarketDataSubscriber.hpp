06b9c995a85b048d7f16788d0f8eeb4a16f393b2
tutumogu <tutumogu@gmail.com>
2017-11-28 11:58:38 +0800

    xxx

--- src/MarketDataSubscriber.hpp
+++ src/MarketDataSubscriber.hpp
@@ -2,6 +2,7 @@
 #define MARKET_DATA_SUBSCRIBER_HPP
 
 #include "ThostFtdcMdApi.h"
+#include "iconvpp.hpp"
 #include <map>
 #include <string>
 #include <fstream>
@@ -12,15 +13,27 @@ class MarketDataSubscriber : public CThostFtdcMdSpi
 {
 public:
   MarketDataSubscriber(CThostFtdcMdApi* pApi, const char* pBrokerId, const char* pUserId, const char* pPassword);
+  
   virtual void OnFrontConnected();
+
   virtual void OnFrontDisconnected(int nReason);
+
   virtual void OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
   virtual void OnRspSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
   virtual void OnRtnDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarketData);
+
+  virtual void OnRspError(CThostFtdcRspInfoField *pRspInfo,
+						int nRequestID, bool bIsLast);
+
+  bool IsErrorRspInfo(CThostFtdcRspInfoField *pRspInfo);
+    
 private:
   CThostFtdcMdApi* mpApi;
   CThostFtdcReqUserLoginField mReq;
-  map<string, ofstream> mInstruments;
+  iconvpp::converter mCvt;
+  iconvpp::converter mCvt2;
   int mRequestId;
 };
 
